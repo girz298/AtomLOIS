5e7f22c8ce68905e386b43d5ad6acef2833dfd43
sega298 <sega298@mail.ru>
2016-03-24 02:03:17 +0300

    some methods

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function(){
+  function findCountOfVar(str){
+    var testStr = str.match(/[A-Z]/g);
+    for (var i = 0; i < testStr.length; i++) {
+        for (var j = i+1; j < testStr.length; j++) {
+          if (testStr[i]===testStr[j]) {
+            delete testStr[j];
+          }
+        }
+    }
+    return testStr.join("").length;
+  }
+  console.log(findCountOfVar("(A^B+C+B+A+DABHTABAB)"));
+
+  function getArrOfBin(count){
+    var arrOfInt = [];
+    for(var i = 0;i<count;i++){
+      arrOfInt.push(i.toString(2));
+    }
+    return arrOfInt.map(function(curr,i,arr){
+      if(curr.length!==arr[arr.length-1].length){
+        curr = Array(arr[arr.length-1].length-curr.length+1)
+        .join("0")+curr;
+        return curr.match(/[01]/g);
+      }
+      return curr.match(/[01]/g);
+    });
+  }
+  console.log(getArrOfBin(Math.pow(2,findCountOfVar("(A^B+C+D)"))));
+
+  // (A+B)*(B+C)+(!(A*B))
+  function getAllSubForm(str){
+    if(str.match(/[(]/g).length!==str.match(/[)]/g).length){   //добавить проверку валидных символов
+      return "Invalid sequence";
+    }
+    var arrOfSubForm = [];
+  }
+
+  String.prototype.replaceToGood = function(){
+    return this.replace("^"," && ").replace("v"," || ")
+    .replace("->"," <= ").replace("~"," == ");
+  }
+
+  console.log(getAllSubForm("(((A+B)*(B+C))+(!(A*B)))"));
+  console.log("(A+B)*(B+C)+(!(A*B))".match(/\(.{0,3}\)/g));
+  console.log("((1^0)->1)~1".replaceToGood());
+  console.log(eval("(((1^1)->0)~0)".replaceToGood())); //goooooooood
+}());
